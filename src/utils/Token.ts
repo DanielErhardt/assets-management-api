957b933d5c9830b26744f09a38a4656abc8e0087
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-12-06 20:51:35 -0300

    chore: added summaries to the Token util methods

--- src/utils/Token.ts
+++ src/utils/Token.ts
@@ -7,13 +7,23 @@ class Token {
 
   private static options: SignOptions = {
     algorithm: 'HS256',
-    expiresIn: '7d', // Long expiration for example project only
+    expiresIn: '7d', // In a real world application tokens should have a very short expiration.
   };
 
+  /**
+   * Creates a new token.
+   * @param payload The user information that shoud be saved in the token.
+   * @returns A valid token containing the payload.
+   */
   public static create(payload: JwtPayload): string {
     return sign(payload, this.secret, this.options);
   }
 
+  /**
+   * Checks the token validity.
+   * @param token A token string.
+   * @returns An object with the payload info encrypted in the token.
+   */
   public static validate(token: string): JwtPayload {
     return verify(token, this.secret) as JwtPayload;
   }
