3d7d9eaf0ff555cf4a4a637d31301ceb0238f2cc
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-12-07 12:35:08 -0300

    fix: added underscore to entities id prop it now matches the MongoDB _id

--- src/@types/Entities/Entity.ts
+++ src/@types/Entities/Entity.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const entityZodType = z.object({
-  id: z.string().optional(),
+  _id: z.string().optional(),
 });
 
 export type Entity = z.infer<typeof entityZodType>;
