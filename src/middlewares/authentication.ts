b7a16a4032f6987187279314ee0250a66601eb25
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-12-06 22:43:27 -0300

    feat: implemented authentication middlewares the same file contains 3 levels of permitions

---
+++ src/middlewares/authentication.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, RequestHandler } from 'express';
+import { UserRole } from '../@types/UserRole';
+import RequestError from '../utils/RequestError';
+import Token from '../utils/Token';
+
+const getAccessLevel = (role: UserRole): number => {
+  const level = { user: 1, manager: 2, admin: 3 };
+  return level[role];
+};
+
+const auth = (req: Request, next: NextFunction, requiredRole: UserRole) => {
+  const { headers: { authorization } } = req;
+
+  if (!authorization) throw RequestError.unauthorized('Token not found.');
+
+  const {
+    id, role,
+  } = Token.validate(authorization);
+
+  if (!role) throw RequestError.badRequest('Token has no role assigned to it.');
+
+  const granted = (getAccessLevel(requiredRole) - getAccessLevel(role)) >= 0;
+
+  if (!granted) throw RequestError.unauthorized(`This route requires ${requiredRole} role.`);
+
+  req.headers.userId = id;
+
+  return next();
+};
+
+const user: RequestHandler = (req, _res, next) => {
+  auth(req, next, 'user');
+};
+
+const manager: RequestHandler = (req, _res, next) => {
+  auth(req, next, 'manager');
+};
+
+const admin: RequestHandler = (req, _res, next) => {
+  auth(req, next, 'admin');
+};
+
+export default { user, manager, admin };
