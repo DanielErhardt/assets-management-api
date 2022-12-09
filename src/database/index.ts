58ffc2ded80f28e59ba25e66ade6c78fdf5ac0c1
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-12-08 23:01:19 -0300

    chore: exported MONGO_URI_DEV on database index created a URI for testing

--- src/database/index.ts
+++ src/database/index.ts
@@ -5,8 +5,10 @@ const {
   DB_USERNAME, DB_PASSWORD, DB_HOST, DB_LOCAL_PORT, DB_NAME,
 } = process.env;
 
-const MONGO_URI = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_LOCAL_PORT}/${DB_NAME}?authSource=admin`;
+export const MONGO_URI_DEV = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_LOCAL_PORT}/${DB_NAME}?authSource=admin`;
 
-export const connect = async (mongoURI = MONGO_URI) => mongoose.connect(mongoURI);
+export const MONGO_URI_TEST = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_LOCAL_PORT}/Testing?authSource=admin`;
+
+export const connect = async (mongoURI = MONGO_URI_DEV) => mongoose.connect(mongoURI);
 
 export const disconnect = async () => mongoose.connection.close();
